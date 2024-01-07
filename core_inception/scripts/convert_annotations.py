c2419897d0acd370f40bc48b89efef6242e0dfe1
Nick Budak <budak@stanford.edu>
2024-01-06 19:42:16 -0800

    Improve error reporting for conversion script

--- core_inception/scripts/convert_annotations.py
+++ core_inception/scripts/convert_annotations.py
@@ -63,36 +63,48 @@ def convert(input_path: Path, output_path: Path, n_sents: int, lang: str):
     matched_filenames = conllu_filenames & conll_filenames
 
     # if the file has both conllu and conll versions, join them, otherwise copy
-    msg.info(f"Grouping {len(all_filenames)} files into a corpus.")
     out_docs = DocBin()
     for filename in all_filenames:
+        conll_file = tmpdir / "conll" / f"{filename}.spacy"
+        conllu_file = tmpdir / "conllu" / f"{filename}.spacy"
         if filename in matched_filenames:
+            msg.info(f"Combining '{filename}.conllu' and '{filename}.conll'")
             conllu_docs = list((
                 DocBin()
-                .from_disk(tmpdir / "conllu" / f"{filename}.spacy")
+                .from_disk(conllu_file)
                 .get_docs(nlp.vocab)
             ))
             conll_docs = list((
                 DocBin()
-                .from_disk(tmpdir / "conll" / f"{filename}.spacy")
+                .from_disk(conll_file)
                 .get_docs(nlp.vocab)
             ))
             for base_doc, ner_doc in zip(conllu_docs, conll_docs):
-                base_doc.ents = ner_doc.ents
-                out_docs.add(base_doc)
-            msg.info(
-                f"Combined '{filename}.conllu' and '{filename}.conll' ({len(conllu_docs)} docs)"
-            )
+                try:
+                    base_doc.ents = ner_doc.ents
+                    out_docs.add(base_doc)
+                except IndexError as e:
+                    msg.fail(f"Failed to merge documents: {e}")
+                    msg.text(f"Document begins:")
+                    msg.text(f"{base_doc.text[:50]}...")
+                    for i, token in enumerate(base_doc):
+                        if token.text != ner_doc[i].text:
+                            msg.text(f"Mismatched token {i}:")
+                            msg.text(f"{token.text} ({filename}.conllu)")
+                            msg.text(f"{ner_doc[i].text} ({filename}.conll)")
+                            break
+                    exit(1)
         else:
             if filename in conllu_filenames:
-                file = tmpdir / "conllu" / f"{filename}.spacy"
+                file = conllu_file
             if filename in conll_filenames:
-                file = tmpdir / "conll" / f"{filename}.spacy"
+                file = conll_file
             docs = list(DocBin().from_disk(file).get_docs(nlp.vocab))
             for doc in docs:
                 out_docs.add(doc)
 
     # write the combined corpus to disk
+    msg.info(f"Grouping {len(all_filenames)} files into a corpus.")
     out_file_path = (output_path / "all.spacy").resolve()
     out_docs.to_disk(out_file_path)
     msg.good(f"Generated corpus ({len(out_docs)} documents): {out_file_path}")
